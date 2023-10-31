ad0345db436e744ec5f39bdcb4543f5dd0cffebc
kenta okamura <side.junktown@gmail.com>
2023-10-31 20:50:59 +0900

    2.24.0

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ADG",
-            url: "https://github.com/AdGeneration/ADG-iOS-SDK/releases/download/2.22.2/ADG.xcframework.zip",
-            checksum: "a24c5cb2111bee35dccaf0e00fc79bbfd77342ff1422f1531739062f0a5e7d03"
+            url: "https://github.com/AdGeneration/ADG-iOS-SDK/releases/download/2.24.0/ADG.xcframework.zip",
+            checksum: "182298c880b19cc56ef508fb24355a32e32859fd31455b5390de6fb575e3a64f"
         ),
     ]
 )
